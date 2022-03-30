d9a829844b550e02fee717d810e0a15d8eb82a25
Arne Neumann <github@arne.cl>
2022-03-30 16:47:42 +0200

    app: add PHOT network output table

--- Program/app.R
+++ Program/app.R
@@ -4,30 +4,50 @@ options(shiny.port = 8181)
 
 source('netwk_anav2.R')
 
+# consensus network
 consensus = read.delim('../Data/consensus0.1.tab', stringsAsFactors = FALSE)
+# PHOT network
+phot = read.delim('../Data/gen3x0.1consens.tab', stringsAsFactors = FALSE)
 
 # TODO: use all gene IDs (consensus and phot network, source and target nodes)
 GeneIds = as.list(unique(consensus$from))
 
 ui <- fluidPage(
+    # app title
     titlePanel("GRN_web"),
-    selectInput(
-      inputId = "geneID",
-      label = "Select gene ID",
-      choices = c("Choose one" = "", GeneIds)),
-    sliderInput(
-      inputId = "num_top_targets",
-      label = "Number of targets",
-      min = 1,
-      max = 100,
-      value = 25),
-    tableOutput(
-      outputId = "topTargets")
+    
+    # sidebar layout (inputs on the left, output on the right)
+    sidebarLayout(
+      
+      # sidebar panel for inputs on the left
+      sidebarPanel(
+        selectInput(
+          inputId = "geneID",
+          label = "Select gene ID",
+          choices = c("Choose one" = "", GeneIds)),
+        sliderInput(
+          inputId = "num_top_targets",
+          label = "Number of targets",
+          min = 1,
+          max = 100,
+          value = 25)
+      ),
+      
+      # main panel for outputs on the right
+      mainPanel(
+        tableOutput(outputId = "topConsensusTargets"),
+        tableOutput(outputId = "topPhotTargets")
+      )
+    )
 )
 
 server <- function(input, output) {
-  output$topTargets = renderTable({
-    top25Targets = regtarget(consensus, input$geneID, input$num_top_targets)
+  output$topConsensusTargets = renderTable({
+    regtarget(consensus, input$geneID, input$num_top_targets)
+  })
+  
+  output$topPhotTargets = renderTable({
+    regtarget(phot, input$geneID, input$num_top_targets)
   })
 }
 
