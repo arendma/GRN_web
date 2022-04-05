bb7b5191cbfcdfdbbd5fa3cf5434e7bab057f901
Suiram23 <marius.arend@fu-berlin.de>
2022-04-05 18:29:06 +0200

    generate set of all targets for mads factors

--- Program/test.R
+++ Program/test.R
@@ -1,5 +1,6 @@
 #Test file retrieving the targets of MADS box regulators in chlamy from the consensus and the g3xcons network
 source('netwk_anav2.R')
+source('cregoenricherv9.r')
 
 #Regulator IDs
 mads_ids=c('Cre11.g467577', 'Cre06.g253250')
@@ -9,9 +10,9 @@ consensus=read.delim('../Data/consensus0.1.tab', stringsAsFactors = FALSE)
 #PHOT network
 phot=read.delim('../Data/gen3x0.1consens.tab', stringsAsFactors = FALSE)
 
-#extract top 25 targets for the two mads tfs in the consensus network
-cons_madstar1=regtarget(consensus,mads_ids[1], 25)
-cons_madstar2=regtarget(consensus, mads_ids[2], 25)
+#extract all targets for the two mads tfs in the consensus network
+cons_madstar1=regtarget(consensus,mads_ids[1])
+cons_madstar2=regtarget(consensus, mads_ids[2])
 
 #extract all targets of the two mads tfs in the PHOT network
 phot_madstar1=regtarget(phot, mads_ids[1])
@@ -21,8 +22,11 @@ phot_madstar2=regtarget(phot, mads_ids[2])
 #extract the the top 25 coregulators of mads 1 regulator targets and plot the network
 #this will create twot plots in pdf format and 1 tsv with label legend for the nodes
 #in the parent directory
-cons_coreg=regTFls(consensus, cons_madstar1$target, 25, '../test')
+cons_coreg=regTFls(consensus, cons_madstar1$target[1:25], 25, '../test')
 
 #find all coregulators for the single highest ranked target genen of mads2 in the phot network
 phot_mads2coreg=regTFs(phot, phot_madstar2$name[1])
+
+## Analyse all targets in the consensus network for GO terms enriched
+cregoenricher(samples = cons_madstar1$target, universe = consensus$to, resdir = './', category = 'BP')
 
