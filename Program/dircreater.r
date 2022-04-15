5144baa53c6be9533f72bdf673bade6f32b371df
Suiram23 <marius.arend@fu-berlin.de>
2022-04-15 16:31:31 +0200

    remove unused dircreater code

--- Program/dircreater.r
+++
@@ -1,6 +0,0 @@
-dircreater <- function(directory, recursive=TRUE) {
-  if (!file.exists(directory)){
-    dir.create(directory, recursive= recursive)
-  }
-  else {print('Directory does already exist')}
-} 
