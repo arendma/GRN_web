d635de3e3d543163e587f70af2529a5fa0481040
Arne Neumann <github@arne.cl>
2022-06-14 15:10:54 +0200

    test.R: add enrichment plots usage examples

--- Program/test.R
+++ Program/test.R
@@ -1,6 +1,7 @@
 #Test file retrieving the targets of MADS box regulators in chlamy from the consensus and the g3xcons network
 source('netwk_anav2.R')
 source('cregoenricherv9.r')
+source('web_ggendotplot.r')
 
 #Regulator IDs
 mads_ids=c('Cre11.g467577', 'Cre06.g253250')
@@ -24,7 +25,7 @@ phot_madstar2=regtarget(phot, mads_ids[2])
 
 #extract the the top 25 coregulators of mads 1 regulator targets and plot the network
 #this will create twot plots in pdf format and 1 tsv with label legend for the nodes
-#in the parent directory
+#in the parent directory (i.e. testlognet.pdf, testnet.pdf and testputreg.tsv)
 cons_coreg=regTFls(consensus, cons_madstar1$target[1:25], 0.08, '../test')
 
 #find 70% of all coregulators for the single highest ranked target genen of mads2 in the phot network
@@ -35,5 +36,11 @@ pdf(NULL)
 
 ## Analyse all targets in the consensus network for GO terms enriched
 res1=cregoenricher(samples = list(cons_madstar1$target), universe = unique(consensus$to), category = 'BP')
-res2=cregoenricher(samples = list(cons_madstar2$target), universe = unique(consensus$to), category = 'BP')
+enrichment_plots1 = web_ggendotplot(res1)
 
+### draw enrichment plots in a grid
+#grid.newpage()
+#grid.draw(cbind(ggplotGrob(enrichment_plots1$heatmap), ggplotGrob(enrichment_plots1$goplot)))
+
+res2=cregoenricher(samples = list(cons_madstar2$target), universe = unique(consensus$to), category = 'BP')
+enrichment_plots2 = web_ggendotplot(res2)
