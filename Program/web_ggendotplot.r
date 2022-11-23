a8418be9dbeeb47a46dbe86648dc1933c5338ad4
Arne Neumann <github@arne.cl>
2022-11-23 14:05:53 +0100

    web_ggendotplot(): generate plot, but don't show it

--- Program/web_ggendotplot.r
+++ Program/web_ggendotplot.r
@@ -1,14 +1,16 @@
 web_ggendotplot <- function(enrichResult, nGO=5) {
-  #function to plot the output from GO enrichment function 
+  #takes the output of the GO enrichment function and creates a plot for it
   #INPUT:
   # - enrichResult: a data frame containing the results of GO enrichment in cluster profiler format
-  # - nGO: (optional, default: 5) the maximum number of GO terms to plot 
-  require(grid)
+  # - nGO: (optional, default: 5) the maximum number of GO terms to plot
   require(ggplot2)
+
   if (anyDuplicated(enrichResult$Description)) {
     print(enrichResult$Description[duplicated(enrichResult$Description)])
   }
+
   enrichResult=enrichResult[order(enrichResult$p.adjust)[1:min(nrow(enrichResult), nGO)],]
+
   #calculate jaccard index of linked genes in each GO set
   jaccard=function(x, y) {
     return(length(intersect(x,y))/length(union(x,y)))
@@ -18,11 +20,13 @@ web_ggendotplot <- function(enrichResult, nGO=5) {
     jaccard_mat[i, i:length(enrichResult$geneID)]=sapply(strsplit(enrichResult$geneID[i:length(enrichResult$geneID)], split='/'), jaccard, y=unlist(strsplit(enrichResult$geneID[i], split='/')))
     jaccard_mat[i:length(enrichResult$geneID),i]=jaccard_mat[i, i:length(enrichResult$geneID)]
   }
-  #cluater according to jaccard inex
+
+  #cluster according to jaccard index
   d=dist(jaccard_mat, method = 'euclidian')
   cl=hclust(d)
   enrichResult=enrichResult[rev(cl$order),]
-  jaccard_mat=jaccard_mat[rev(cl$order),rev(cl$order)] 
+  jaccard_mat=jaccard_mat[rev(cl$order),rev(cl$order)]
+
   plot_tab=data.frame(x=rep(enrichResult$Description, each=nrow(jaccard_mat)), y=rep(enrichResult$Description, nrow(jaccard_mat)), value=as.vector(jaccard_mat))
   hm=ggplot(plot_tab, aes(x, y, fill=value)) + geom_tile() + labs(fill="Jaccard Idx")+ scale_x_discrete(limits=enrichResult$Description, position='top') + scale_y_discrete(limits=enrichResult$Description) +
     theme(legend.position = "left", text= element_text(size=10), axis.title= element_blank(), axis.text.x=element_text(angle=90, hjust=0))
@@ -31,11 +35,14 @@ web_ggendotplot <- function(enrichResult, nGO=5) {
   xlimits <- c(min(plotdat$Gene_Ratio)- 0.5*min(plotdat$Gene_Ratio), max(plotdat$Gene_Ratio)+0.3*max(plotdat$Gene_Ratio))
   goplot <- ggplot(data=plotdat, aes(x=Gene_Ratio, y=desc, color=padj, size=count))+ geom_point() + scale_x_continuous(limits = xlimits) + theme_light() +
     scale_colour_gradient(low = "red", high = "blue") + theme(text = element_text(size=10),axis.title.y=element_blank(), axis.text.y=element_blank()) + scale_size(range=c(9,15))
-    
-  grid.newpage()
-  grid.draw(cbind(ggplotGrob(hm), ggplotGrob(goplot)))
-  
   result <- list("goplot" = goplot, "heatmap"= hm)
   return(result)
+}
+
+print_go_enrichment_plot <- function(go_enrichment_plot) {
+  require(grid)
+
+  grid.newpage()
+  grid.draw(cbind(ggplotGrob(go_enrichment_plot$heatmap), ggplotGrob(go_enrichment_plot$goplot)))
 }
 
