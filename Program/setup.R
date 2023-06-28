1543e5db496c7ae6a92596555ea660d71ecb3c59
Arne Neumann <github@arne.cl>
2023-06-28 11:33:24 +0200

    setup.R: replace deprecated AnVIL::install

--- Program/setup.R
+++ Program/setup.R
@@ -8,16 +8,12 @@ for (pkg in packages) {
   }
 }
 
-# Install bioconductor packages as binary packages
-# instead of compiling them locally. This should be
-# much faster.
-if(!require('AnVIL')) {
-  remotes::install_github("Bioconductor/AnVIL")
-}
+# Install bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
 for (bioc_pkg in bioc_packages) {
-  if(!(requireNamespace(bioc_pkg, quietly=TRUE))) {
-    library(AnVIL)
-    AnVIL::install(bioc_pkg)
+  if (!(requireNamespace(bioc_pkg, quietly = TRUE))) {
+    BiocManager::install(bioc_pkg)
   }
 }
